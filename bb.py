4db09097359676a387bbe76f41156bac5246f320
saracaksan <saracaksan@gmail.com>
2026-05-30 02:02:13 +0300

    Update bb.py

--- bb.py
+++ bb.py
@@ -24,7 +24,7 @@ try:
 except Exception:
     GEMINI_API_KEY = "YOK" # Kasa bulunamazsa uygulamanın tamamen çökmesini önler
 
-GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key={GEMINI_API_KEY}"
+    GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key={GEMINI_API_KEY}"
 
 
 # ==========================================
