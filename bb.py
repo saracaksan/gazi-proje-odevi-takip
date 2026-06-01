c2d7d5c3e1e1953d813e1be507a6cc8500bb4fd3
saracaksan <saracaksan@gmail.com>
2026-06-01 18:13:05 +0300

    Update bb.py

--- bb.py
+++ bb.py
@@ -498,7 +498,6 @@ def yonetim_paneli(df, ayarlar):
                         d_k = {f"{k['id']}_puan": st.session_state[f"vp_{k['id']}"], f"{k['id']}_aciklama": st.session_state[f"va_{k['id']}"]} for k in aktif_sablon
                         d_k_flat = {}
                         for k in aktif_sablon: d_k_flat.update({f"{k['id']}_puan": st.session_state[f"vp_{k['id']}"], f"{k['id']}_aciklama": st.session_state[f"va_{k['id']}"]})
-                        
                         df.at[idx, 'Dinamik_JSON'] = json.dumps(d_k_flat, ensure_ascii=False)
                         df.at[idx, 'Genel Değerlendirme Yorumu'] = gv
                         df.at[idx, 'Toplam Puan'] = toplam_h
